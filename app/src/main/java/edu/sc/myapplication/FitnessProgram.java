eb1dfbcff2592a32551d78f1c2252bc3b9c753fa
Owner <sandifee@email.sc.edu>
2015-10-20 21:07:03 -0400

    Button Code

--- app/src/main/java/edu/sc/myapplication/FitnessProgram.java
+++ app/src/main/java/edu/sc/myapplication/FitnessProgram.java
@@ -1,35 +1,22 @@
 package edu.sc.myapplication;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends Activity {
-
+public class FitnessProgram extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button yourButton = (Button)findViewById(R.id.button);
-        yourButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, FitnessProgram.class));
-            }
-        });
-
+        setContentView(R.layout.activity_fitness_program);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_fitness_program, menu);
         return true;
     }
 
