710bb5d8787f90640e7708843015686b19148f92
Ben <thoma464@email.sc.edu>
2015-11-11 19:40:35 -0500

    Modified Fragments

--- app/src/main/java/edu/sc/myapplication/FP_Body_Building_Fragment.java
+++ app/src/main/java/edu/sc/myapplication/FP_Body_Building_Fragment.java
@@ -1,20 +1,19 @@
 package edu.sc.myapplication;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FP_STR_TrainingFragment extends Fragment {
-
-
+public class FP_Body_Building_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fp__str__training, container, false);
+        return inflater.inflate(R.layout.fragment_fp__body__building, container, false);
     }
-
-
 
 }
--- app/src/main/java/edu/sc/myapplication/FitnessProgramFragment.java
+++ app/src/main/java/edu/sc/myapplication/FitnessProgramFragment.java
@@ -1,20 +1,51 @@
 package edu.sc.myapplication;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class FitnessProgramFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_fp__str__training, container, false);
+        Button btnstrength = (Button) v.findViewById(R.id.StrengthTrainingButton);
+        btnstrength.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FP_STR_TrainingFragment fragment = new FP_STR_TrainingFragment();
+                FragmentManager fm = getFragmentManager(); //or getFragmentManager() if you are not using support library.
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, fragment);
+                ft.commit();
+            }
+        });
+
+        Button btnbuilding = (Button) v.findViewById(R.id.BodyBuildingButton);
+        btnbuilding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FP_Body_Building_Fragment fragment1 = new FP_Body_Building_Fragment();
+                FragmentManager fm = getFragmentManager(); //or getFragmentManager() if you are not using support library.
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, fragment1);
+                ft.commit();
+            }
+        });
         // Inflate the layout for this fragment
       //  View v = inflater.inflate(R.layout.fragment_fitness_program, null);
 
         return inflater.inflate(R.layout.fragment_fitness_program, container, false);
+
+
     }
 
 
