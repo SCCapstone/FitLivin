6753b5bb9aa239804c380075b1e9dc2651840e50
Ben <thoma464@email.sc.edu>
2015-11-16 22:05:55 -0500

    Organized Layout of Body Building

--- app/src/main/java/edu/sc/myapplication/FP_STR_TrainingFragment.java
+++ app/src/main/java/edu/sc/myapplication/FP_STR_TrainingFragment.java
@@ -1,18 +1,34 @@
 package edu.sc.myapplication;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class FP_STR_TrainingFragment extends Fragment {
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_fp__str__training, container, false);
+        Button btn = (Button) v.findViewById(R.id.button9);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FitnessProgramFragment fragment1 = new FitnessProgramFragment();
+                FragmentManager fm = getFragmentManager(); //or getFragmentManager() if you are not using support library.
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, fragment1);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fp__str__training, container, false);
+        return v;
     }
 
 
