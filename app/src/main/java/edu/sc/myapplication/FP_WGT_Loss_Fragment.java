6e0cc7ce3224af121558ffdd6b4ae8c6e3d7909a
Ben <thoma464@email.sc.edu>
2015-11-17 00:42:15 -0500

    Added functionality to back button for weight loss.

--- app/src/main/java/edu/sc/myapplication/FP_WGT_Loss_Fragment.java
+++ app/src/main/java/edu/sc/myapplication/FP_WGT_Loss_Fragment.java
@@ -2,12 +2,15 @@ package edu.sc.myapplication;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class FP_WGT_Loss_Fragment extends Fragment {
 
@@ -15,6 +18,20 @@ public class FP_WGT_Loss_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fp__wgt__loss, container, false);
+        View v = inflater.inflate(R.layout.fragment_fp__wgt__loss, container, false);
+        Button btn = (Button) v.findViewById(R.id.backButtonWeightLoss);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FitnessProgramFragment fragment1 = new FitnessProgramFragment();
+                FragmentManager fm = getFragmentManager(); //or getFragmentManager() if you are not using support library.
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, fragment1);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+        // Inflate the layout for this fragment
+        return v;
     }
 }
