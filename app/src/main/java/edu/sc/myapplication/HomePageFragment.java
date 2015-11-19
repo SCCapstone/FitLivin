3d9b2186ff9f9a082fd2e359c557662c4b4da7fc
Ben <thoma464@email.sc.edu>
2015-11-19 15:26:08 -0500

    Cleaned up interface and minor debugging

--- app/src/main/java/edu/sc/myapplication/HomePageFragment.java
+++ app/src/main/java/edu/sc/myapplication/HomePageFragment.java
@@ -23,7 +23,7 @@ public class HomePageFragment extends Fragment{
         Log.d("T", "ONcREATEVIEW");
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home_page, container, false);
-        Button btn = (Button) v.findViewById(R.id.button);
+        Button btn = (Button) v.findViewById(R.id.fitnessProgramButton);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
