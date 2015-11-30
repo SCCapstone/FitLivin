1ee5f2fb936e592184c50e052e65317c139d64fd
Owner <sandifee@email.sc.edu>
2015-11-30 15:58:54 -0500

    Added page transfer code for weightloss

---
+++ app/src/main/java/edu/sc/myapplication/WeightLossDayTwo.java
@@ -0,0 +1,47 @@
+package edu.sc.myapplication;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class WeightLossDayTwo extends Fragment {
+
+
+    public WeightLossDayTwo() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_weightloss_day3, container, false);
+        Button backBtn = (Button) v.findViewById(R.id.WLBack);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FPWGTLossFragment fragment1 = new FPWGTLossFragment();
+                FragmentManager fm = getFragmentManager(); //or getFragmentManager() if you are not using support library.
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, fragment1);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
+
+        return v;
+    }
+
+
+
+}
+
