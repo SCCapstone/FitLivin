82e7dbcaf00dcfd6fd792eb24a2164a07f1039a4
Parth Choksi <pkchoksi39@gmail.com>
2016-01-13 00:44:44 -0500

    Made some minor changes and centralized Database initialization

---
+++ app/src/main/java/edu/sc/FitLivin/Application.java
@@ -0,0 +1,17 @@
+package edu.sc.FitLivin;
+
+import com.parse.Parse;
+
+/**
+ * Created by pkcho on 1/13/2016.
+ * This class initializes parse and registers subclass.
+ * We can add as many subclasses we want to this file
+ * this will be initialized as soon as the app starts.
+ */
+public class Application extends android.app.Application{
+    public void onCreate() {
+        super.onCreate();
+        Parse.enableLocalDatastore(this);//enables the database
+        Parse.initialize(this, getString(R.string.parse_application_Id), getString(R.string.parse_application_Key));
+    }
+}
