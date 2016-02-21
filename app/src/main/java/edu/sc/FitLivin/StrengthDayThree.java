a9d012833559f771bb6b050d1844fc87637911af
Zachary R. Lawson <lawsonz@email.sc.edu>
2016-02-21 15:03:16 -0500

    Strength Day 3, 4, and 5 points button now functional

--- app/src/main/java/edu/sc/FitLivin/StrengthDayThree.java
+++ app/src/main/java/edu/sc/FitLivin/StrengthDayThree.java
@@ -124,6 +124,16 @@ MediaPlayer mp;
             }
         });
 
+        Button complete = (Button) v.findViewById(R.id.completeDay3s);//creates complete button
+        complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer points = MainActivity.points;
+                points = points + 50;//adds points for completed workout
+                MainActivity main = new MainActivity();
+                main.pointsData(points);
+            }
+        });
 
         return v;
     }
