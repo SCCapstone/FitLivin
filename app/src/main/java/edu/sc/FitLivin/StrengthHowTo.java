4b3a4b8e47008d1c39ab6f23e82f27626ca63211
Zachary R. Lawson <lawsonz@email.sc.edu>
2016-03-02 21:06:20 -0500

    Strength & Bodybuilding How to with back button functionality

--- app/src/main/java/edu/sc/FitLivin/StrengthHowTo.java
+++ app/src/main/java/edu/sc/FitLivin/StrengthHowTo.java
@@ -8,13 +8,15 @@
 package edu.sc.FitLivin;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class StrengthHowTo extends Fragment {
@@ -31,7 +33,21 @@ public class StrengthHowTo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_strength_how_to, container, false);
+        View v = inflater.inflate(R.layout.fragment_strength_how_to, container, false);
+
+        Button backBtn = (Button) v.findViewById(R.id.Sdayhow2back);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FPSTRTrainingFragment fragment1 = new FPSTRTrainingFragment();
+                FragmentManager fm = getFragmentManager(); //or getFragmentManager() if you are not using support library.
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, fragment1);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+        return v;
     }
 
 
