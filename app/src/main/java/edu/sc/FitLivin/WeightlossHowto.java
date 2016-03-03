ddae1f6c7ffd89001121e0c029040691324db332
Zachary R. Lawson <lawsonz@email.sc.edu>
2016-03-02 21:21:08 -0500

    Strength & Bodybuilding How to with back button functionality

---
+++ app/src/main/java/edu/sc/FitLivin/WeightlossHowto.java
@@ -0,0 +1,54 @@
+package edu.sc.FitLivin;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import edu.sc.FitLivin.R;
+
+public class WeightlossHowto extends Fragment {
+
+
+    public WeightlossHowto() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_weightloss_howto, container, false);
+
+        Button backBtn = (Button) v.findViewById(R.id.Wlhowtoback);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FPWGTLossFragment fragment1 = new FPWGTLossFragment();
+                FragmentManager fm = getFragmentManager(); //or getFragmentManager() if you are not using support library.
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, fragment1);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+        return v;
+    }
+    }
+
+
+
+
+
