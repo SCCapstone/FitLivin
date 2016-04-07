eba1bf98c9d5693f463d9c4bcbb0d7a1561bf5ff
Parth Choksi <pkchoksi39@gmail.com>
2016-04-06 22:39:31 -0400

    Advanced Camera Added

--- app/src/main/java/edu/sc/FitLivin/CameraActivity.java
+++ app/src/main/java/edu/sc/FitLivin/CameraActivity.java
@@ -1,55 +1,194 @@
 package edu.sc.FitLivin;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.hardware.Camera;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.desmond.squarecamera.ImageUtility;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by pkcho on 3/2/2016.
  */
-public class CameraActivity extends FragmentActivity {
-    private Camera mCamera = null;
-    private CameraView mCameraView = null;
+public class CameraActivity extends AppCompatActivity {
+    private static final int REQUEST_CAMERA = 0;
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private Point mSize;
+    private EditText titleText;
+    private Button Done;
+    private Intent newdata;
+    private Button SaveProfile;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+        setContentView(R.layout.activity_newcamera);
 
-        try{
-            mCamera = Camera.open();//you can use open(int) to use different cameras
-        } catch (Exception e){
-            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+
+
+        Done = (Button)findViewById(R.id.Done);
+
+
+
+
+
+
+        Display display = getWindowManager().getDefaultDisplay();
+        mSize = new Point();
+        display.getSize(mSize);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Done = (Button)findViewById(R.id.Done);
+
+        if (resultCode != RESULT_OK) return;
+
+        if (requestCode == REQUEST_CAMERA) {
+            final Uri photoUri = data.getData();
+            ParseUser user = ParseUser.getCurrentUser();
+            // Get the bitmap in according to the width of the device
+            final Bitmap bitmap = ImageUtility.decodeSampledBitmapFromPath(photoUri.getPath(), mSize.x, mSize.x);
+            ((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
+            Done.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try{
+                        if(photoUri != null){
+                            final ProgressDialog dlg = new ProgressDialog(CameraActivity.this);
+                            dlg.setTitle("Please wait.");
+                            dlg.setMessage("Saving Picture.  Please wait.");
+                            dlg.show();
+                            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                            byte[] byteArray = stream.toByteArray();
+                            ParseFile photofile = new ParseFile("Picture.jpg", byteArray);
+                            ParseUser curruser = ParseUser.getCurrentUser();
+                            curruser.put("Images",photofile);
+                            curruser.saveInBackground(new SaveCallback() {
+                                @Override
+                                public void done(ParseException e) {
+
+                                    if (e == null){
+                                        dlg.dismiss();
+                                        Toast.makeText(CameraActivity.this, "Profile Picture set", Toast.LENGTH_SHORT).show();
+                                        startActivityForResult(new Intent(CameraActivity.this, MainActivity.class), 0);
+                                    }
+                                    else {
+                                        Toast.makeText(CameraActivity.this, "Error..please check your connection", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(CameraActivity.this, "Please take a picture first", Toast.LENGTH_SHORT).show();
+                        }
+                    }catch (Exception error){
+                        error.printStackTrace();
+                    }
+                }
+            });
+
         }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
-        if(mCamera != null) {
-            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
-            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
-            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+    public void requestForCameraPermission(View view) {
+        final String permission = Manifest.permission.CAMERA;
+        if (ContextCompat.checkSelfPermission(CameraActivity.this, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(CameraActivity.this, permission)) {
+                showPermissionRationaleDialog("Test", permission);
+            } else {
+                requestForPermission(permission);
+            }
+        } else {
+            launch();
         }
+    }
 
-        //btn to close the application
-        Button takepic = (Button) findViewById(R.id.takeimage);
-        takepic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    private void showPermissionRationaleDialog(final String message, final String permission) {
+        new AlertDialog.Builder(CameraActivity.this)
+                .setMessage(message)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        CameraActivity.this.requestForPermission(permission);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-                Toast.makeText(getApplicationContext(),"Please wait until Beta 2.0 before camera works",Toast.LENGTH_LONG).show();
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            }
+                    }
+                })
+                .create()
+                .show();
+    }
 
-        });
-        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
-        imgClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.exit(0);
-            }
-        });
+    private void requestForPermission(final String permission) {
+        ActivityCompat.requestPermissions(CameraActivity.this, new String[]{permission}, REQUEST_CAMERA_PERMISSION);
+    }
+
+    private void launch() {
+        Intent startCustomCameraIntent = new Intent(this, com.desmond.squarecamera.CameraActivity.class);
+        startActivityForResult(startCustomCameraIntent, REQUEST_CAMERA);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION:
+                final int numOfRequest = grantResults.length;
+                final boolean isGranted = numOfRequest == 1
+                        && PackageManager.PERMISSION_GRANTED == grantResults[numOfRequest - 1];
+                if (isGranted) {
+                    launch();
+                }
+                break;
+
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    public  byte[] scaledData(byte[] data){
+        // Resize photo from camera byte array
+        Bitmap imageProfile = BitmapFactory.decodeByteArray(data, 0, data.length);
+        final Bitmap imageProfileScaled = Bitmap.createScaledBitmap(imageProfile, 500, 500
+                * imageProfile.getHeight() / imageProfile.getWidth(), false);
+
+
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        imageProfileScaled.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+
+
+        byte[] scaledData = bos.toByteArray();
+        return scaledData;
     }
 }
